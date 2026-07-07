2b4d78cacfa14b006e382c36b8dd04a7b01d0d94
Flebber <SpareSpare413@gmail.com>
2026-07-07 15:30:06 +0100

    Created Spawn Point Scnee (no logic yet)
    Fixed offseting area bug on obstacles and points

--- Scripts/LevelScripts/Points/point_manager.gd
+++ Scripts/LevelScripts/Points/point_manager.gd
@@ -4,7 +4,10 @@ class_name PointManager extends Node2D
 @export var point: Area2D
 
 func setup():
-	collision_check.collided.connect(point_touched())
+	if collision_check == null:
+		print("No collision_check for point_manager")
+		return
+	collision_check.collided.connect(point_touched)
 	
 
 # When a flag gets touched = save data of player touching, play flag anamation, 
