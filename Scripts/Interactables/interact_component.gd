59740502f29d3f42456bc09771f5397f12b47688
Flebber <SpareSpare413@gmail.com>
2026-07-31 15:16:39 +0100

    Interact Prompt Logic Created, Tested If Modular
    NEed to make custom font for the UI, Slapped both components onto a random are2d test node and it works. Modular Label Nameing is also implemented

--- Scripts/Interactables/interact_component.gd
+++ Scripts/Interactables/interact_component.gd
@@ -1,38 +1,63 @@
 class_name InteractComponent extends Node
 
+@onready var parent : Area2D = get_parent()
 var collision_check : CollisionCheck
-var interact_prompt
+
+const PROMPT = preload("uid://dweg3gd21eops")
+var prompt_instance : Prompt 
+@export var PromptText : String
+
 
 var interactable : bool = false
-
 signal interact_pressed
 
 func setup():
+	if parent == null:
+		print("No Parent for Interact")
+		return
+	
+	if collision_check == null:
+		print("No CollisionCheck for Interact")
+		return
+		
 	collision_check.collided.connect(interact_available)
 	collision_check.body_exited.connect(interact_disable)
-
-
-func _process(_delta: float) -> void:
-	interact_activate()
 
 # If Player is near Display Interact Prompt and Allow Interact to be pressed {Connected to collision_check.collided}
 func interact_available(_body):
 	interactable = true
 	print("Interact is Available")
-	
-	# Display Interact UI
-	# If Player Exists Parents Area, Disable Interact Prompt 
+	create_prompt()
 
-func interact_activate():
-	if interactable == false:
+
+# If interact is available And Interact is presssed
+func _input(event: InputEvent) -> void:
+	if not interactable:
 		return
 	
-	elif Input.is_action_just_pressed("interact"):
+	if event.is_action_pressed("interact"):
 		print("interacted")
 		interact_pressed.emit()
 
 
+# If Body exited the parent
 func interact_disable(_body):
 	interactable = false
 	print("Interact is not Available")
+	
+	if prompt_instance:
+		prompt_instance.queue_free()
+		prompt_instance = null
+
+
+# Instantiate the Prompt with a Custom Label
+func create_prompt():
+	if prompt_instance:
+		return
+	
+	prompt_instance = PROMPT.instantiate()
+	prompt_instance.label.text = PromptText
+	prompt_instance.position = parent.position
+	prompt_instance.position.y = prompt_instance.position.y - 100
+	add_child(prompt_instance)
 	
---
+++ Scripts/UI/prompt.gd
@@ -0,0 +1,4 @@
+class_name Prompt extends Control
+
+@export var label: Label
+@export var key_icon: TextureRect
