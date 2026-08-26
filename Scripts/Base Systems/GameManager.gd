6a5b5ab5bd7f04a5634195b3a856c6a815e62c27
Flebber <SpareSpare413@gmail.com>
2026-08-26 22:23:54 +0100

    Imported Font to godot
    Font works much better with margins and sizing now.

--- Scripts/Base Systems/GameManager.gd
+++ Scripts/Base Systems/GameManager.gd
@@ -1,12 +1,15 @@
 extends Node
 
 # Level/Scene Signals/Flags
+@warning_ignore("unused_signal")
 signal level_finished
 var is_level_finished : bool = false
+@warning_ignore("unused_signal")
 signal scene_swapped(scene : PackedScene)
 
 
 # Player Signals/Flags
+@warning_ignore("unused_signal")
 signal player_dead
 var can_player_move : bool = true
 
--- Scripts/LevelScripts/scene_manager.gd
+++ Scripts/LevelScripts/scene_manager.gd
@@ -12,9 +12,9 @@ var levels : Dictionary[String, PackedScene] = {
 # Assign Menu levels to seperate dicitonary
 var menu_levels : Dictionary[String, PackedScene] = {
 	"Test":  load("res://Scenes/Levels/test_level.tscn"),
-	"Menu": load("res://Scenes/Levels/Menus/menu.tscn"),
-	"Death Menu": load("res://Scenes/Levels/Menus/death_menu.tscn"),
-	"Finish Menu": load("res://Scenes/Levels/Menus/finish_menu.tscn"),
+	"Menu": load("res://Scenes/UI/Menus/menu.tscn"),
+	"Death Menu": load("res://Scenes/UI/Menus/death_menu.tscn"),
+	"Finish Menu": load("res://Scenes/UI/Menus/finish_menu.tscn"),
 }
 
 # Global function to change current scene to the specified (scene), Also tracks last/current scene if it was a level
