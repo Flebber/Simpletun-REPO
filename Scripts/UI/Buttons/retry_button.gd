2260262fb823d1e418203b6ea99ee8621ccf4650
Flebber <SpareSpare413@gmail.com>
2026-09-15 17:14:24 +0100

    Fixed Retry Bug
    simplified retry button to reload the current scene, rather than load the presious scene. IDK why i had it that way

--- Scripts/UI/Buttons/retry_button.gd
+++ Scripts/UI/Buttons/retry_button.gd
@@ -6,7 +6,5 @@ func _ready() -> void:
 
 # When pressed, Reload the last scene loaded, If there is no previous scene, load the current scene too mitigate load bug {Connected to button_down.connect}
 func retry_pressed() -> void:
-	if SceneManagerNode.previous_scene == null:
-		SceneManagerNode.scene_select(SceneManagerNode.current_scene)
-	SceneManagerNode.scene_select(SceneManagerNode.previous_scene)
+	SceneManagerNode.scene_select(SceneManagerNode.current_scene)
 	
