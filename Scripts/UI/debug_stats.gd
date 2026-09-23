90cec498f9b3d8c3998fd0aa21ddff5ef9c23ff2
Flebber <SpareSpare413@gmail.com>
2026-09-23 23:55:33 +0100

    Finished Debug UI
    Displays using label and String.format()
    Checkpoint: ({hascheckp}, {checkpos})
    health: {health}
    velocity: {speed}
    can_glide: {gcheck}

--- Scripts/UI/debug_stats.gd
+++ Scripts/UI/debug_stats.gd
@@ -4,16 +4,17 @@ extends Control
 @export var movement: Movement
 @export var health: Health
 @export var glide_check: RayCast2D
+@export var player: Player
 
 func _physics_process(delta: float) -> void:
 	var hascheckp : bool = GameManager.has_checkpoint
 	var checkpos : Vector2 = GameManager.checkpointPos
-	var p_health : int = health.current_health
-	var speed : float = movement.speed
-	var gcheck : bool = glide_check.is_colliding()
+	var p_health : float = health.current_health
+	var speed : Vector2 = player.velocity
+	var gcheck : bool = !glide_check.is_colliding()
 	stats.text = str(
-		"Checkpoint: (, pos)
-		health: 
-		speed:
-		glide_check: "
-	)
+		"Checkpoint: ({hascheckp}, {checkpos})
+		health: {health} 
+		velocity: {speed}
+		can_glide: {gcheck}"
+	).format({"hascheckp": hascheckp, "checkpos": checkpos, "health": p_health, "speed": speed, "gcheck": gcheck})
