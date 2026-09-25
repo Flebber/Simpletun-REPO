11c9d68c068c2fff8e18ce7d813d3417332233fe
Flebber <SpareSpare413@gmail.com>
2026-09-25 19:58:29 +0100

    Inventory now resets from reset_level_data func

--- Scripts/Base Systems/main.gd
+++ Scripts/Base Systems/main.gd
@@ -2,11 +2,13 @@ class_name Main extends Node
 
 func _ready() -> void:
 	GameManager.level_cancelled.connect(reset_level_data)
-	
-	if GameManager.is_cancel_connect == false:
-		GameManager.level_cancelled.connect(InventoryManager.reset_count)
-		GameManager.is_cancel_connect = true
+	#
+	##Connects Inventory 
+	#if GameManager.is_cancel_connect == false:
+		#GameManager.level_cancelled.connect(InventoryManager.reset_count)
+		#GameManager.is_cancel_connect = true
 	pass
 
 func reset_level_data():
 	GameManager.has_checkpoint = false
+	InventoryManager.reset_count()
